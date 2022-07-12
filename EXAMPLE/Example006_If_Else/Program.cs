f4d97da8fb2abae9b71d5948aa849d1895120ac5
marina chichaeva <aliska827@gmail.com>
2022-07-12 22:35:41 +0300

    Example006_if_else

---
+++ EXAMPLE/Example006_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+  Console.WriteLine("Ура, это Марина");
+}
+else
+{
+ Console.WriteLine("Привет, ");
+ Console.WriteLine(username); 
+}
